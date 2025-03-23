b51322bf62169134a1ee5aec057391c11550d7a6
Kontolistik <151897038+Hexamouse@users.noreply.github.com>
2025-03-23 13:39:05 +0700

    change animate aos in about

--- components/About.tsx
+++ components/About.tsx
@@ -12,7 +12,7 @@ export default function About() {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </p>
           </div>
-          <div data-aos="fade-left">
+          <div data-aos="fade-up">
             <Image
               src="/assets/etc/logo/logo.png"
               alt="KSR PMI Activity"
